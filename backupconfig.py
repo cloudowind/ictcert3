736c1716eaacc8aca8e6132cc5c43e7c71396b18
EC2 Default User <ec2-user@ip-172-31-44-84.ap-southeast-2.compute.internal>
2023-09-19 06:56:07 +0000

    noting

---
+++ backupconfig.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+jobs = { "job700" :"/home/ec2-user/environment/file1.txt" , "job1": "/home/ec2-user/environment/ictcert3/"} 
+
+thisDir = "/home/ec2-user/environment/backupconfig.py"
+destinationDir = "backup"
+backupLog ="/home/ec2-user/environment/ictprg302-t1/backup.log"
+##email={}
